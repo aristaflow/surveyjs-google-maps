c9c9fb18a463e1c2779ba5dad4350d45553468da
Ulrich Kreher <Ulrich.Kreher@aristaflow.com>
2023-10-02 10:10:58 +0200

    Support underscore as separator in address fields.

--- src/surveyjs-google-maps-widget.js
+++ src/surveyjs-google-maps-widget.js
@@ -10,7 +10,7 @@ var addressautocomplete = {
         return question.getType() === 'addressautocomplete';
     },
     activatedByChanged: function (activatedBy) {
-        Survey.JsonObject.metaData.addClass("addressautocomplete", [], null, "text");
+        Survey.Serializer.addClass("addressautocomplete", [], null, "text");
     },
     isDefaultRender: true,
     // add Google Maps Autocomplete to the rendered text field
@@ -19,8 +19,7 @@ var addressautocomplete = {
         var text;
         if (el.tagName.toLowerCase() !== 'input') {
             text = el.getElementsByTagName("input")[0];
-        }
-        else {
+        } else {
             text = el;
         }
         text.className = "sd-input sd-text sv-text";
@@ -65,6 +64,7 @@ var addressautocomplete = {
      * name, if available. Target questions will be searched in the designated parent element using
      * the following (question) names:
      * NameOfAutoCompleteQuestion.AddressFieldName
+   * NameOfAutoCompleteQuestion_AddressFieldName
      * NameOfAutoCompleteQuestionAddressFieldName
      * AddressFieldName
      *
@@ -74,6 +74,10 @@ var addressautocomplete = {
         var targetName = thisQuestionName + '.' + fieldName;
         var cq = parent.getQuestionByName(targetName);
         if (!cq) {
+      targetName = thisQuestionName + '_' + fieldName;
+      cq = parent.getQuestionByName(targetName);
+    }
+    if (!cq) {
           targetName = thisQuestionName + fieldName;
           cq = parent.getQuestionByName(targetName);
         }
@@ -86,6 +90,34 @@ var addressautocomplete = {
             cq.value = address[fieldName];
         }
     },
+
+  /**
+   * Transfers data from the given address object to the designated target question (expected to be
+   * the addressautocomplete question) and the designated target input if appropriate. This checks
+   * for whether one of the designated question fields (paramName, valueName, name) contains a
+   * string that represents a (possibly prefixed) field name of the designated address:
+   * NameOfAutoCompleteQuestion.AddressFieldName
+   * NameOfAutoCompleteQuestionAddressFieldName
+   * AddressFieldName
+   *
+   * @return Whether the designated address field has been assigned to the designated target
+   *         question and input.
+   */
+  transferValueToAcQuestion: function(address, qFields, aFieldName, targetQuestion, targetInput) {
+    var ret = false;
+    for (qFieldName of qFields) {
+      if (qFieldName && (qFieldName == targetQuestion.name + '.' + aFieldName
+                         || qFieldName == targetQuestion.name + aFieldName
+                         || qFieldName == aFieldName)) {
+        targetInput.value = address[aFieldName];
+        targetQuestion.value = address[aFieldName];
+        ret = true;
+        break;
+      }
+    }
+    return ret;
+  },
+
     initMap: function(input, position, question) {
         var autocomplete = new google.maps.places.Autocomplete(input);
 
@@ -127,10 +159,13 @@ var addressautocomplete = {
             var address = {
                 streetName: streetName,
                 StreetName: streetName,
+                street_name: streetName,
                 streetNumber: streetNumber,
                 StreetNumber: streetNumber,
+                street_number: streetNumber,
                 postalCode: postalCode,
                 PostalCode: postalCode,
+                postal_code: postalCode,
                 neighborhood: neighborhood,
                 Neighborhood: neighborhood,
                 sublocality: sublocality,
@@ -139,42 +174,38 @@ var addressautocomplete = {
                 City: city,
                 administrativeArea: administrativeArea,
                 AdministrativeArea: administrativeArea,
+                administrative_area: administrativeArea,
                 countryShort: countryShort,
                 CountryShort: countryShort,
+                country_short: countryShort,
                 country: country,
                 Country: country,
                 street: streetName + ((streetNumber) ?  ' ' + streetNumber : ''),
                 Street: streetName + ((streetNumber) ?  ' ' + streetNumber : ''),
                 formattedAddress: formattedAddress,
-                FormattedAddress: formattedAddress
+                FormattedAddress: formattedAddress,
+                formatted_address: formattedAddress
             };
-            console.info("Retrieved address data:", place.address_components)
+            console.info("Retrieved address data:", place.address_components);
             console.info("Formatted address: %s", place.formatted_address);
             console.info("SurveyJS address data:", address);
 
             var survey = question.survey;
             var valueSet = false;
-            // transfer all values to questions, if they exist
-            Object.getOwnPropertyNames(address).forEach(function(fieldName) {
+            // transfer all values (address parts) to questions, if they exist
+            Object.keys(address).forEach(function(fieldName) {
                 if (!question.parent) {
                     that.transferValue(address, survey, fieldName, question.name)
                 } else {
                     that.transferValue(address, question.parent, fieldName, question.name)
                 }
 
-                // prefer the value name over the question name
-                if (!valueSet && question.name + fieldName == question.valueName) {
-                  input.value = address[fieldName];
-                  question.value = address[fieldName];
-                  valueSet = true;
-                }
-                // only use the question name if no valueName is set
-                else if (!valueSet && !question.valueName && fieldName == question.name) {
-                  input.value = address[fieldName];
-                  question.value = address[fieldName];
-                  valueSet = true;
-                }
+                valueSet ||= that.transferValueToAcQuestion(address, [question.paramName, question.valueName, question.name],
+                                                            fieldName, question, input);
             });
+
+            // if neither paramName nor valueName nor question name specify the fieldName, set value of
+            // addressautocomplete question to the formatted address
             if (!valueSet) {
               input.value = place.formatted_address;
               question.value = place.formatted_address;
@@ -193,7 +224,6 @@ try {
     google.maps.places.Autocomplete;
 
     Survey.CustomWidgetCollection.Instance.addCustomWidget(addressautocomplete, "addressautocomplete");
-}
-catch(e) {
+} catch(e) {
     console.info("Google Autocomplete not loaded. Probably no valid API-Key provided.");
 }
